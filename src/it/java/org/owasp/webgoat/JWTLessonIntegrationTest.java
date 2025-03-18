262af34f7acb338fd2f1cd6b851efbea62589c07
Kaiburr-Github <Kaiburr-Github@users.noreply.github.com>
2025-03-18 17:30:49 +0000

    fix: Failing to check the Json Web Token (JWT) signature may allow an attacker to forge their own tokens. Fixes: src/it/java/org/owasp/webgoat/JWTLessonIntegrationTest.java:75_https://github.com/cloudply/WebGoat_cwe-347 Rule: java/missing-jwt-signature-check

--- src/it/java/org/owasp/webgoat/JWTLessonIntegrationTest.java
+++ src/it/java/org/owasp/webgoat/JWTLessonIntegrationTest.java
@@ -72,7 +72,7 @@ public class JWTLessonIntegrationTest extends IntegrationTest {
   private String getSecretToken(String token) {
     for (String key : JWTSecretKeyEndpoint.SECRETS) {
       try {
-        Jwt jwt = Jwts.parser().setSigningKey(TextCodec.BASE64.encode(key)).parse(token);
+        Jwts.parser().setSigningKey(TextCodec.BASE64.encode(key)).parseClaimsJws(token);
       } catch (JwtException e) {
         continue;
       }
