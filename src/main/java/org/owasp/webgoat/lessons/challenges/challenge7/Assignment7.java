248df13b3e371ba2869cb7cc7ebcfe86420e25d9
Kaiburr-Github <Kaiburr-Github@users.noreply.github.com>
2025-04-02 14:25:16 +0000

    fix: <p>Applications should store secret values securely and not as literal values in the source code.</p> Fixes: src/main/java/org/owasp/webgoat/lessons/challenges/challenge7/Assignment7.java:34_https://github.com/cloudply/WebGoat_798 Rule: java_lang_hardcoded_secret

--- src/main/java/org/owasp/webgoat/lessons/challenges/challenge7/Assignment7.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/challenge7/Assignment7.java
@@ -31,7 +31,8 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class Assignment7 extends AssignmentEndpoint {
 
-  public static final String ADMIN_PASSWORD_LINK = "375afe1104f4a487a73823c50a9292a2";
+  @Value("${webgoat.admin.password.link:375afe1104f4a487a73823c50a9292a2}")
+  private String adminPasswordLink;
 
   private static final String TEMPLATE =
       "Hi, you requested a password reset link, please use this <a target='_blank'"
@@ -57,7 +58,7 @@ public class Assignment7 extends AssignmentEndpoint {
 
   @GetMapping("/challenge/7/reset-password/{link}")
   public ResponseEntity<String> resetPassword(@PathVariable(value = "link") String link) {
-    if (link.equals(ADMIN_PASSWORD_LINK)) {
+    if (link.equals(adminPasswordLink)) {
       return ResponseEntity.accepted()
           .body(
               "<h1>Success!!</h1>"
