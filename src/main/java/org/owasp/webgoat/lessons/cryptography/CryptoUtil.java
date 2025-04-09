6743e5ecea702c3ebc1a12ce3ea840f11ca9d847
Kaiburr-Github <Kaiburr-Github@users.noreply.github.com>
2025-04-09 01:59:22 +0000

    fix: <p>Hard-coding secrets in a project opens them up to leakage. This rule checks for common secret types such as keys, tokens, and passwords using the popular Gitleaks library and ensures they aren't hard-coded. This rule is part of the secrets scanner and language agnostic.</p> Fixes: src/main/java/org/owasp/webgoat/lessons/cryptography/CryptoUtil.java:133_https://github.com/cloudply/WebGoat_798 Rule: gitleaks

--- src/main/java/org/owasp/webgoat/lessons/cryptography/CryptoUtil.java
+++ src/main/java/org/owasp/webgoat/lessons/cryptography/CryptoUtil.java
@@ -29,6 +29,9 @@ public class CryptoUtil {
     BigInteger.valueOf(257),
     BigInteger.valueOf(65537)
   };
+  
+  private static final String BEGIN_PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----";
+  private static final String END_PRIVATE_KEY = "-----END PRIVATE KEY-----";
 
   public static KeyPair generateKeyPair()
       throws NoSuchAlgorithmException, InvalidAlgorithmParameterException {
@@ -42,14 +45,14 @@ public class CryptoUtil {
   }
 
   public static String getPrivateKeyInPEM(KeyPair keyPair) {
-    String encodedString = "-----BEGIN PRIVATE KEY-----\n";
+    String encodedString = BEGIN_PRIVATE_KEY + "\n";
     encodedString =
         encodedString
             + new String(
                 Base64.getEncoder().encode(keyPair.getPrivate().getEncoded()),
                 Charset.forName("UTF-8"))
             + "\n";
-    encodedString = encodedString + "-----END PRIVATE KEY-----\n";
+    encodedString = encodedString + END_PRIVATE_KEY + "\n";
     return encodedString;
   }
 
@@ -130,8 +133,8 @@ public class CryptoUtil {
 
   public static PrivateKey getPrivateKeyFromPEM(String privateKeyPem)
       throws NoSuchAlgorithmException, InvalidKeySpecException {
-    privateKeyPem = privateKeyPem.replace("-----BEGIN PRIVATE KEY-----", "");
-    privateKeyPem = privateKeyPem.replace("-----END PRIVATE KEY-----", "");
+    privateKeyPem = privateKeyPem.replace(BEGIN_PRIVATE_KEY, "");
+    privateKeyPem = privateKeyPem.replace(END_PRIVATE_KEY, "");
     privateKeyPem = privateKeyPem.replace("\n", "").replace("\r", "");
 
     byte[] decoded = Base64.getDecoder().decode(privateKeyPem);
