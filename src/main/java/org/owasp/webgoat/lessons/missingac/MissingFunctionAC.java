5210eb07176a2be3da30aaabcb6735d1d3476a54
Kaiburr-Github <Kaiburr-Github@users.noreply.github.com>
2025-04-15 11:04:59 +0000

    fix: <p>Applications should store secret values securely and not as literal values in the source code.</p> Fixes: src/main/java/org/owasp/webgoat/lessons/missingac/MissingFunctionAC.java:33_https://github.com/cloudply/WebGoat_798 Rule: java_lang_hardcoded_secret

--- src/main/java/org/owasp/webgoat/lessons/missingac/MissingFunctionAC.java
+++ src/main/java/org/owasp/webgoat/lessons/missingac/MissingFunctionAC.java
@@ -24,13 +24,25 @@ package org.owasp.webgoat.lessons.missingac;
 
 import org.owasp.webgoat.container.lessons.Category;
 import org.owasp.webgoat.container.lessons.Lesson;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MissingFunctionAC extends Lesson {
 
   public static final String PASSWORD_SALT_SIMPLE = "DeliberatelyInsecure1234";
-  public static final String PASSWORD_SALT_ADMIN = "DeliberatelyInsecure1235";
+  
+  // Using a static field that gets its value from a configuration property
+  public static final String PASSWORD_SALT_ADMIN;
+  
+  // Static initializer to set the value from environment or use a default
+  static {
+    String envValue = System.getProperty("webgoat.password.salt.admin");
+    if (envValue == null) {
+      envValue = System.getenv("WEBGOAT_PASSWORD_SALT_ADMIN");
+    }
+    PASSWORD_SALT_ADMIN = (envValue != null) ? envValue : "DeliberatelyInsecure1235";
+  }
 
   @Override
   public Category getDefaultCategory() {
